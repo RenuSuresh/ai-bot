d7109940a2fe632d36762d14a2b8a1bbff567431
RenuPE101 <renu.ganvir@pharmeasy.in>
2025-03-21 00:32:21 +0530

    revert package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	trailingSlash: true, // Optional: Add trailing slashes to URLs
+	async headers() {
+		return [
+			{
+				source: "/_next/static/(.*)",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public, max-age=31536000, immutable",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
